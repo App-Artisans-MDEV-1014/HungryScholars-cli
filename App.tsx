1a5821573f834134a97ab742427019c6f463671a
Vivek-G-Iyer <iyervivek36@gmail.com>
2023-11-09 19:21:32 -0500

    Convert from Expo to CLI

--- App.tsx
+++ App.tsx
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import auth from '@react-native-firebase/auth';
 
+import RestaurantMenuScreen from './src/view/Menu/RestaurantMenuScreen';
 import HomeScreen from './src/view/Home/Home';
-import RestaurantCard from './src/view/RestaurantCard/RestaurantCard';
 import PaymentMethodSection from './src/view/PaymentMethod/PaymentMethodSection';
 import CheckoutScreen from './src/view/Checkout/CheckoutScreen';
 import ProfileScreen from './src/view/Profile/ProfileScreen';
@@ -15,12 +14,9 @@ import StarRating from './src/view/StarRating/StarRating';
 import LaunchingScreen from './src/view/LaunchingScreen/LaunchingScreen';
 import { Text, View } from 'react-native';
 import LoginScreen from './src/view/auth/LoginScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import RegisterScreen from './src/view/auth/RegisterScreen';
 
 
-
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 // Define the User type based on your project's Firebase user object structure
@@ -43,47 +39,34 @@ function App() {
     });
 
     return unsubscribe; // Unsubscribe when the component unmounts
-
-
   }, []);
 
   return (
-    <NavigationContainer  >
-      {user ? (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarActiveTintColor: 'blue',
-            tabBarInactiveTintColor: 'gray',
-            tabBarLabelStyle: {
-              fontSize: 14,
-              fontWeight: 'bold',
-            },
-            tabBarStyle: {
-              backgroundColor: 'white',
-            },
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="RestaurantCard" component={RestaurantCard} />
-          <Tab.Screen name="PaymentMethod" component={PaymentMethodSection} />
-          <Tab.Screen name="Checkout" component={CheckoutScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-          <Tab.Screen name="Item" component={Item} />
-          <Tab.Screen name="StarRating" component={StarRating} />
-          <Tab.Screen name="Launching" component={LaunchingScreen} />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator>
-     
-
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        {/* Define other screens here if needed */}
+    <NavigationContainer>
+   
+      <Stack.Navigator
+      screenOptions={{
+        headerShown:false,
+      }}>
+        {user ? (
+          <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="RestaurantMenuScreen" component={RestaurantMenuScreen} />
+            <Stack.Screen name="PaymentMethod" component={PaymentMethodSection} />
+            <Stack.Screen name="Checkout" component={CheckoutScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Item" component={Item} />
+            <Stack.Screen name="StarRating" component={StarRating} />
+            <Stack.Screen name="Launching" component={LaunchingScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
       </Stack.Navigator>
-        
-  
-      )}
+    
     </NavigationContainer>
   );
 }
